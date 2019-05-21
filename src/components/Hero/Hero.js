6a4f4ce63fc16e64c98e2ea384ede19e92e7fd13
stobin <stobin@stobin.net>
2019-05-21 21:59:03 +0200

    Add props as component properties

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import styles from './Hero.scss';
 
-const Hero = () => (
+const Hero = props => (
   <div>
     <header className={styles.component}>
-      <h2 className={styles.title}>Things to do</h2>
+      <h2 className={styles.title}>{props.titleText}</h2>
       <img className={styles.image} src="https://uploads.kodilla.com/bootcamp/fer/11.react/space.png" alt="image" />
     </header>
   </div>
